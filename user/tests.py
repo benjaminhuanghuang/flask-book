6aae333e7e38ebe8318a6078678a67b13a09b34e
Benjamin Huang <benjaminhuanghuang@gmail.com>
2017-08-21 09:37:49 -0700

    login

--- user/tests.py
+++ user/tests.py
@@ -1,6 +1,7 @@
 from app import create_app as create_app_base
 from mongoengine.connection import _get_db
 import unittest
+from flask import session
 
 from user.models import User
 
@@ -34,3 +35,26 @@ class UserTest(unittest.TestCase):
             confirm="test123"
         ), follow_redirects=True)
         assert User.objects.filter(username='jorge').count() == 1
+
+    def user_dict(self):
+        return dict(
+            first_name="Jorge",
+            last_name="Escobar",
+            username="jorge",
+            email="jorge@example.com",
+            password="test123",
+            confirm="test123"
+        )
+
+    def test_login_user(self):
+        # create user
+        self.app.post('/register', data=self.user_dict())
+        # login user
+        rv = self.app.post('/login', data=dict(
+            username=self.user_dict()['username'],
+            password=self.user_dict()['password']
+        ))
+        # check the session is set
+        with self.app as c:
+            rv = c.get('/')
+            assert session.get('username') == self.user_dict()['username']
